0536ba304f91722a231c5d0dc9bedaa23829b7d1
Trung Nguyen <128338386+ravenpwn@users.noreply.github.com>
2023-10-10 21:38:28 +0700

    init the app

---
+++ Week3-DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.diceroller
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.button2)
+        rollButton.setOnClickListener {
+            val resultTextView: TextView = findViewById(R.id.textView)
+            resultTextView.text = rollDice()
+            val toast: Toast = Toast.makeText(this,"You rolled a ${resultTextView.text}!", Toast.LENGTH_SHORT)
+            toast.show()
+        }
+
+    }
+
+    private fun rollDice(): String {
+        val dice = Dice(6)
+        return dice.roll().toString()
+
+    }
+}
+
+class Dice(private val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
